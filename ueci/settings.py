41dd754192058c18d56332b350c1e8d47e7e24a0
Ahlan Pimentel Guarnier <ahlan.guarnier@sesp.es.gov.br.local>
2020-05-20 12:07:24 -0300

    ajustando o statick

--- ueci/settings.py
+++ ueci/settings.py
@@ -13,8 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,3 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join('static'), )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
